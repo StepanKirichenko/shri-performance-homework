e4420725c3d2cb47d95d2f6b0ca6c08541b0ee90
StepanKirichenko <kirichenkostepan@yandex.ru>
2023-07-06 16:54:49 +0300

    Add base to viteconfig

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useMemo, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 
 function Header() {
   let [expanded, setExpanded] = useState(false);
